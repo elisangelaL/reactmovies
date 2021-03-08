c8ec9c407d53bae979eaaff2b5a0693e0564ff93
Elisangela L <elimacxx@gmail.com>
2021-03-08 15:50:23 -0300

    Add files via upload

---
+++ build/precache-manifest.b734ac9d7d35b76ec5d30b89861c147d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0cfb97cc1d761f497d7",
+    "url": "/static/css/main.38325c1f.chunk.css"
+  },
+  {
+    "revision": "e0cfb97cc1d761f497d7",
+    "url": "/static/js/main.7d8dca4a.chunk.js"
+  },
+  {
+    "revision": "dc818ddc35c9bc7ffefb",
+    "url": "/static/js/runtime~main.4b123df7.js"
+  },
+  {
+    "revision": "0c9203a85ebf6b9bc22c",
+    "url": "/static/js/2.b12e9ffb.chunk.js"
+  },
+  {
+    "revision": "8e8c09e8561212f1f2cb",
+    "url": "/static/js/3.8490d774.chunk.js"
+  },
+  {
+    "revision": "04d7d41ef405e8c7ab1b03b2fd5f7edd",
+    "url": "/index.html"
+  }
+];
